87aaacbbc1b0641a21af0219ac8158b02d7c0828
le Bao Tu <contact@lebaotu.com>
2022-11-15 11:17:02 +0700

    add lai code

---
+++ Business/ItemProduct/ItemProductModel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ItemProduct
+{
+    public class ItemProductModel
+    {
+        public string StyleCode { get; set; }
+
+        public int Pairs { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string ImageProduct { get; set; }
+
+        /// <summary>
+        /// chất lượng sản phẩm 
+        /// </summary>
+        public int ProQuality { get; set; }
+
+        /// <summary>
+        /// trọng lượng vàng 
+        /// </summary>
+        public float Gold_WT { get; set; }
+
+        /// <summary>
+        /// trọng lượng đá 
+        /// </summary>
+        public float Stone_WT { get; set; }
+
+        /// <summary>
+        /// vàng dòng , độ nguyên chất 
+        /// </summary>
+        public float Net_Gold { get; set; }
+
+        /// <summary>
+        /// tổng trọng lượng 
+        /// </summary>
+        public float Total_Gross_WT { get; set; }
+
+        /// <summary>
+        /// tỉ giá vàng 
+        /// </summary>
+        public float Gold_Rate { get; set; }
+
+        /// <summary>
+        /// số lượng vàng trong mục 
+        /// </summary>
+        public float Gold_Amt { get; set; }
+
+        /// <summary>
+        /// phí làm vàng 
+        /// </summary>
+        public float? Gold_Making { get; set; }
+
+        /// <summary>
+        /// phí làm đá 
+        /// </summary>
+        public float? Stone_Making { get; set; }
+
+        /// <summary>
+        /// các khoản phí khách
+        /// </summary>
+        public float Other_Making { get; set; }
+
+        /// <summary>
+        /// tổng phí thực hiện 
+        /// </summary>
+        public float Total_Making { get; set; }
+
+        /// <summary>
+        /// mô tả 
+        /// </summary>
+        public string Detail { get; set; }
+
+        public string MRP { get; set; }
+
+        /// <summary>
+        ///khóa ngoại đến bảng danh mục cha 
+        /// </summary>
+        public Guid Category_id { get; set; }
+
+        /// <summary>
+        ///khóa ngoại tham chiếu đến bảng Brands 
+        /// </summary>
+        public Guid BrandId { get; set; }
+
+        /// <summary>
+        /// khóa ngoại tham chiếu đến bảng certify(chứng nhận)
+        /// </summary>
+        public Guid CertifyId { get; set; }
+
+        /// <summary>
+        /// khóa ngoại tham chiếu đến bảng danh mục con 
+        /// </summary>
+        public Guid? SubCategoryId { get; set; }
+
+        /// <summary>
+        /// khóa ngoại tham chiếu đến bảng loại vàng (goldk)
+        /// </summary>
+        public Guid GoldTypeId { get; set; }
+
+      
+    }
+
+    public class ItemProductPageModel
+    {
+        public int? PageSize { get; set; }
+
+        public int? PageNumber { get; set; }
+    }
+}
---
+++ eProjectAPI/Controllers/ItemProductController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.ItemProduct;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shared;
+
+namespace eProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemProductController : ControllerBase
+    {
+        private IItemProductHandller _itemProductHandller;
+        private readonly ILogger<ItemProductController> _logger;
+
+        public ItemProductController(IItemProductHandller itemProductHandller, ILogger<ItemProductController> logger)
+        {
+            _itemProductHandller = itemProductHandller;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// lấy danh sách sản phẩm 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("getAllItemProduct")]
+        [Authorize]
+        [ProducesResponseType(typeof(ResponseObject<List<ItemProductModel>>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllItemProduct(ItemProductPageModel model)
+        {
+            return Ok(await _itemProductHandller.GetAllProduct(model));
+        }
+
+        /// <summary>
+        /// Thêm mới sản phẩm 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("insertItemProduct")]
+        [Authorize]
+        [ProducesResponseType(typeof(ResponseObject<ItemProductModel>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> InsertItemProduct(ItemProductModel model)
+        {
+            return Ok(await _itemProductHandller.CreateProduct(model));
+        }
+
+        /// <summary>
+        /// Cập nhật sản phẩm 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("updateItemProduct")]
+        [Authorize]
+        [ProducesResponseType(typeof(ResponseObject<ItemProductModel>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> UpdateItemProduct(ItemProductModel model)
+        {
+            return Ok(await _itemProductHandller.UpdateProduct(model));
+        }
+
+        /// <summary>
+        /// xóa thương hiệu 
+        /// </summary>
+        /// <param name="cateId"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("deleteItemProduct")]
+        [Authorize]
+        [ProducesResponseType(typeof(ResponseObject<String>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteItemProduct(string proId)
+        {
+            return Ok(await _itemProductHandller.DeleteProductById(proId));
+        }
+
+        /// <summary>
+        /// tìm kiếm theo Id 
+        /// </summary>
+        /// <param name="cateId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("getItemProductById")]
+        [Authorize]
+        [ProducesResponseType(typeof(ResponseObject<Guid>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetItemProductById(string proId)
+        {
+            return Ok(await _itemProductHandller.GetProductById(proId));
+        }
+
+
+        /// <summary>
+        /// tìm kiếm theo Name
+        /// </summary>
+        /// <param name="cateId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("getItemProductByName")]
+        [Authorize]
+        [ProducesResponseType(typeof(ResponseObject<List<ItemProductModel>>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetItemProductByName(string proName)
+        {
+            return Ok(await _itemProductHandller.GetproductByName(proName));
+        }
+    }
+}
